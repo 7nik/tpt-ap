f1ecf1fc74dccc54e8dd082b3f1a91c753177a9c
7nik <kifiranet@gmail.com>
2025-11-17 11:22:02 +0200

    fix #2

--- tpt-ap.user.js
+++ tpt-ap.user.js
@@ -53,7 +53,7 @@
 // @require      https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.0.8/dist/floating-ui.dom.umd.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore.js
 // @require      https://github.com/evazion/translate-pixiv-tags/raw/lib-20221207/lib/tooltip.js
-// @require      https://github.com/evazion/translate-pixiv-tags/raw/lib-20240111/lib/jquery-gm-shim.js
+// @require      https://github.com/evazion/translate-pixiv-tags/raw/lib-20250707/lib/jquery-gm-shim.js
 // @require      https://github.com/evazion/translate-pixiv-tags/raw/master/translate-pixiv-tags.user.js?v=1
 // @resource     danbooru_icon https://github.com/evazion/translate-pixiv-tags/raw/resource-20190903/resource/danbooru-icon.ico
 // @resource     settings_icon https://github.com/evazion/translate-pixiv-tags/raw/resource-20190903/resource/settings-icon.svg
